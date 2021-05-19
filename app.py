b86415012c78f618a39574c5aecbbab4c208e7e7
viniciusasf <viniciusasferreira@hotmail.com>
2021-05-19 17:27:43 -0300

    tabulate

--- app.py
+++ app.py
@@ -1,152 +1,184 @@
 import sqlite3
-
-# from time import sleep
 from tabulate import tabulate
-
-# from collections import Counter
-# import sys
-
-conn = sqlite3.connect('contacorente.db')
+import sys
+from time import sleep
 
 
-def database():
+def sair():
+    print('Encerrando o sistema. \nAguarde...\nGravando Dados no Bando de Dados...')
+    contagem()
+    print('Sistema Encerrado')
+    sys.exit()
+
+
+def contagem():
+    for cont in range(3, -1, -1):
+        print(cont, '... ', end='')
+        sleep(1)
+
+
+def criar_conta_corrente():
+    print('')
+    print('++++++++++ CRIAR CONTA-CORRENTE ++++++++++')
+    print('')
     conn = sqlite3.connect('contacorente.db')
     c = conn.cursor()
-    c.execute('''create table if not exists cliente  (id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                                                      nome TEXT, 
-                                                      cidade TEXT, 
-                                                      telefone TEXT)''')
-    c.execute('''create table if not exists conta    (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                                      cod_cliente INTEGER,                                                      
-                                                      saida REAL,
-                                                      entrada REAL,
-                                                      saldo REAL,
-                                                      tr INTEGER,
-                                                      comprovante INTEGER,
-                                                      FOREIGN KEY (cod_cliente) REFERENCES cliente (id)
-                                                      )''')
-    c.execute('''create table if not exists transacao (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                                      id_conta INTEGER,                                                      
-                                                      deposito REAL,
-                                                      saque REAL,
-                                                      saldo REAL,
-                                                      FOREIGN KEY (id_conta) REFERENCES conta (id)
-                                                      )''')
-    conn.commit()
-    conn.close()
+    sql = c.execute('''SELECT * FROM cliente''')
+    consulta = sql.fetchall()
+    head = [col[0] for col in sql.description]
+    print(tabulate(consulta, headers=head, tablefmt="grid"))
+    input('INFORME O ID DO CLIENTE: ')
+    # INSERT NA TABELA DE CONTA COM O ID DO CLIENTE
 
 
 def novo_cliente():
+    print('')
+    print('+++++ Cadastro de Clientes +++++')
+    print('')
     conn = sqlite3.connect('contacorente.db')
     c = conn.cursor()
-    print('------- 1 - CADASTRO CLIENTE / C.C ---------------\n')
     nome = input('NOME DO CLIENTE: ')
     cidade = input('CIDADE: ')
     telefone = input('TELEFONE: ')
-
     c.execute('''INSERT INTO cliente (nome, cidade, telefone)
                 VALUES(?, ?, ?)''', (nome, cidade, telefone))
     conn.commit()
     print('{} Cadastrado com Sucesso'.format(nome))
     conn.close()
+    return print('{} Cadastrado com Sucesso'.format(nome))
 
 
-def clientes():
+def consulta_todos_clientes():
+    print('')
+    print('+++++ Consulta TODOS Clientes +++++')
+    print('')
     conn = sqlite3.connect('contacorente.db')
     c = conn.cursor()
-    sql = '''SELECT * FROM cliente'''
-    c.execute(sql)
-    for l in c.fetchall():
-        print(l)
-    print('')
-    print('Quantidade de Clientes: {}'.format(len(c.execute(sql).fetchall())))
-    conn.close()
-    monta_menu(menu_cadastro)
+    sql = c.execute('''SELECT * FROM cliente''')
+    consulta = sql.fetchall()
+    if consulta:
+        head = [col[0] for col in sql.description]
+        print(tabulate(consulta, headers=head, tablefmt="grid"))
+        print('Quantidade de Clientes: {}'.format(len(consulta)))
+        print('')
+        conn.close()
+        input("Prescione ENTER para sair.")
+    else:
+        print('Nenhum cliente encontrado')
+        conn.close()
 
 
 def consulta_cliente_id():
+    print('')
+    print('+++++ Consulta Cliente por ID +++++')
+    print('')
     conn = sqlite3.connect('contacorente.db')
     c = conn.cursor()
-    sql = "SELECT * FROM cliente WHERE id=?"
-    id = input('Qual o numero do ID_Cliente?: ')
+    id = int(input('Informe o numero do ID_Cliente: '))
     print('')
-    c.execute(sql, [(id)])
-    print(c.fetchall())
-    monta_menu(menu_cadastro)
+    sql = c.execute('''SELECT id, nome, cidade, telefone FROM cliente WHERE id=?''', (id,))
+    consulta = sql.fetchall()
+    if consulta:
+        head = [col[0] for col in sql.description]
+        print(tabulate(consulta, headers=head, tablefmt="grid"))
+        input("Prescione ENTER para sair.")
+    else:
+        print(f'ID numero {id} não localizado.')
+        print('')
+        consulta_cliente_id()
+        print('')
+
+    conn.close()
 
 
 def consulta_cliente_nome():
+    print('')
+    print('+++++ Consulta Cliente NOME +++++')
+    print('')
     conn = sqlite3.connect('contacorente.db')
     c = conn.cursor()
-    sql = "SELECT * FROM cliente WHERE nome LIKE '%'||?||'%'"
-    nome = input('Qual o nome do cliente?: ')
-    c.execute(sql, ([nome]))
-    print(c.fetchall())
-    monta_menu(menu_cadastro)
+    nome = input('Informe o nome do cliente: ')
+    sql = c.execute("SELECT * FROM cliente WHERE nome LIKE '%'||?||'%'", (nome,))
+    print('')
+    consulta = sql.fetchall()
+    if consulta:
+        head = [col[0] for col in sql.description]
+        print(tabulate(consulta, headers=head, tablefmt="grid"))
+        input("Prescione ENTER para sair.")
+    else:
+        print(f'Cliente {nome} não localizado(a).')
+        print('')
+        consulta_cliente_nome()
+        print('')
+
+    conn.close()
 
 
 def consulta_cliente_cidade():
+    print('')
+    print('+++++ Consulta Cliente por Cidade +++++')
+    print('')
     conn = sqlite3.connect('contacorente.db')
     c = conn.cursor()
-    sql = "SELECT * FROM cliente WHERE cidade LIKE '%'||?||'%'"
-    cidade = input('Qual a Cidade do Cliente: ')
-    c.execute(sql, ([cidade]))
-    print(c.fetchall())
-    monta_menu(menu_cadastro)
-
-
-def consulta_cliente():
-    print('Contalta de Clientes, escolha as opções abaixo: ')
-    selecao = '''
-    1 - ID
-    2 - Nome
-    3 - Cidade
-    4 - Todos
-    '''
-    print(selecao)
-    menu = input('Digite uma das Opções: ')
-
-    if menu == '1':
+    cidade = input('Informe a Cidade do Cliente: ')
+    sql = c.execute("SELECT * FROM cliente WHERE cidade LIKE '%'||?||'%'", (cidade,))
+    consulta = sql.fetchall()
+    if cidade:
+        head = [col[0] for col in sql.description]
+        print(tabulate(consulta, headers=head, tablefmt="grid"))
+        conn.close()
+        input("Prescione ENTER para sair.")
+    else:
+        print(f' Cidade {cidade} não foi localizado.')
         print('')
-        print('+++++ Consulta por ID_Cliente +++++')
-        consulta_cliente_id()
+        conn.close()
 
 
-    if menu == '2':
-        print('')
-        print('+++++ Consulta por NOME_Cliente +++++')
-        consulta_cliente_nome()
+def transDeposito():
+    print('')
+    print('+++++ DEPÓSITO +++++')
+    print('')
+    input('Sua Identificação: Informe o seu ID: ')
+    input('Selecione o cliente para realizar o depósito: ')
+    print('')
+    print('CAIXA EM MANUTENÇÃO')
 
-    if menu == '3':
-        print('')
-        print('+++++ Consulta por CIDADE_Cliente +++++')
-        consulta_cliente_nome()
 
-    if menu == '4':
-        print('')
-        print('+++++ Consulta TODOS_Clientes +++++')
-        clientes()
+def transSaque():
+    pass
+
+
+def transFerencia():
+    pass
+
+
+def pagamentos():
+    pass
+
+
+def extrato():
+    pass
 
 
 menu_cadastro = {
-    '1': ('- Cadastro', novo_cliente),
-    #'2': ('- Lista', clientes),
-    #'3': ('- Consulta por ID', consulta_cliente_id),
-    '2': ('- Consulta Geral', consulta_cliente),
-    #'9': ('- Deslogar', sair),
+    '1': ('- Cadastro de Cliente', novo_cliente),
+    '2': ('- Consulta por ID', consulta_cliente_id),
+    '3': ('- Consulta por Cidade', consulta_cliente_cidade),
+    '4': ('- Consulta Todos Clientes', consulta_todos_clientes),
+    '9': ('- Deslogar', sair),
 }
-# menu_trans = {
-#    '1': ('- Depósito', transDeposito),
-#    '2': ('- Saque', transSaque),
-#    '3': ('- Transferência', transFerencia),
-#    '4': ('- Pagamentos', pagamentos),
-#    '5': ('- Extrato', extrato),
-#    '9': ('- Voltar', menu_cadastro),
-# }
+menu_trans = {
+    '1': ('- Depósito', transDeposito),
+    '2': ('- Saque', transSaque),
+    '3': ('- Transferência', transFerencia),
+    '4': ('- Pagamentos', pagamentos),
+    '5': ('- Extrato', extrato),
+    '9': ('- Voltar', menu_cadastro),
+}
 menu_principal = {
-    '1': ('- Clientes', menu_cadastro),
-    #    '2': ('- Transações', menu_trans),
-    #    '9': ('- Deslogar', sair),
+    '1': ('- Cadastros', menu_cadastro),
+    '2': ('- Transações - Em desenvolvimento', menu_trans),
+    '9': ('- Deslogar - Em desenvolvimento', sair),
 }
 
 
@@ -173,4 +205,5 @@ def monta_menu(menu) -> object:
 
 
 if __name__ == '__main__':
-    monta_menu(menu_principal)
+    criar_conta_corrente()
+    # monta_menu(menu_principal)
